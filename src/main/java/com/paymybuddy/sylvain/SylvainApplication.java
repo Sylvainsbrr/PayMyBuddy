f173d3e0550b622272294344a9f65050bc93644b
SylvainSbrr <sylvainsouber@hotmail.fr>
2021-11-01 16:29:13 +0100

    init

---
+++ src/main/java/com/paymybuddy/sylvain/SylvainApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.sylvain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylvainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SylvainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymybuddy/sylvain/SylvainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.sylvain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SylvainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
